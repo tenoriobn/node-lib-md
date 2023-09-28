f01537e9a1e858dfb675b56bfcea865ae0b96252
Bruno Tenório <tenoriobn@gmail.com>
2023-09-28 15:13:30 -0300

    criando função para extrair links do texto

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-const textoTeste = 'A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
-
 function extraiLinks(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}));
 
-  const capturas = regex.exec(texto);
-
-  console.log(capturas)
+  return resultados;
 }
-
-extraiLinks(textoTeste)
 
 function trataErro(erro) {
   console.log(erro)
@@ -24,7 +20,7 @@ async function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
   
-    console.log(chalk.green(texto))
+    console.log(extraiLinks(texto));
   } catch(erro) {
     trataErro(erro)
   } finally {
@@ -32,7 +28,4 @@ async function pegaArquivo(caminhoDoArquivo) {
   }
 }
 
-// pegaArquivo('./arquivos/texto.md');
-
-
-// \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
+pegaArquivo('./arquivos/texto.md');
