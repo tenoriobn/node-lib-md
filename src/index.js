a1c222bee1c352a4bb805055f12060a966c6115d
Bruno Tenório <tenoriobn@gmail.com>
2023-10-02 20:14:20 -0300

    Organizando entradas e saídas

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function extraiLinks(texto) {
   const capturas = [...texto.matchAll(regex)];
   const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}));
 
-  return resultados;
+  return resultados.length !== 0 ? resultados : 'Não há links no arquivo';
 }
 
 function trataErro(erro) {
@@ -20,7 +20,7 @@ async function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
   
-    console.log(extraiLinks(texto));
+    return extraiLinks(texto);
   } catch(erro) {
     trataErro(erro)
   } finally {
