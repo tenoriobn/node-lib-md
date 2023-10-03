238b568b7c33c673c413cb61bdaa5b53501dca82
Bruno Tenório <tenoriobn@gmail.com>
2023-10-03 10:28:05 -0300

    tratando novos erros

--- src/cli.js
+++ src/cli.js
@@ -4,12 +4,25 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
-function imprimeLista(resultado) {
-  console.log(chalk.yellow('Lista de links'), resultado)
+function imprimeLista(resultado, identificador = '') {
+  console.log(
+    chalk.yellow('Lista de links'), 
+    chalk.black.bgGreen(identificador),
+    resultado
+  );
 }
 
 async function processaTexto(argumentos) {
   const caminho = argumentos[2];
+
+  try{
+    fs.lstatSync(caminho);
+  } catch (erro) {
+    if (erro.code === "ENOENT") {
+      console.log('Arquivo ou diretório não existe');
+      return;
+    }
+  }
 
   if (fs.lstatSync(caminho).isFile()) {
     const resultado = await pegaArquivo(argumentos[2]);
@@ -20,7 +33,7 @@ async function processaTexto(argumentos) {
     arquivos.forEach(async (nomeDeArquivo) => {
       const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
 
-      imprimeLista(lista)
+      imprimeLista(lista, nomeDeArquivo)
     })
   }
 
